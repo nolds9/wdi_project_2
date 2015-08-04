bd8256d2f1c95b2c114f7abdd12379b8e43afbd3
Nicholas Olds <nolds9@yahoo.com>
2015-08-04 17:49:51 -0400

    Added model validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,8 @@
 class Course < ActiveRecord::Base
   has_many :lessons, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :content, presence: true, length: { maximum: 500 }
+  
+
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,9 @@
 class Lesson < ActiveRecord::Base
-  belongs_to :project
+  belongs_to :course
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :video, presence: true
+  validates :tag, presence: true
+  validates :course, presence: true
+
 end
