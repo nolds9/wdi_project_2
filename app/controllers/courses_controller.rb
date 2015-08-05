0c503d2f173385855ffb4cf74671d7f4c84fdad3
Nicholas Olds <nolds9@yahoo.com>
2015-08-04 22:32:42 -0400

    Worked on views and got courses index to display

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,17 +1,14 @@
 class CoursesController < ApplicationController
-  before_action :find_course, only: [:show]
+
   def index
     @courses = Course.all
   end
 
   def show
-    @lessons = @course.lessons
+    @course = Course.find(params[:id])
+  	@lessons = @course.lessons
   end
 
   private
-  def find_course
-    @course = Course.find(params[:id])
-
-  end
 
 end
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,7 +1,7 @@
 class LessonsController < ApplicationController
   def show
-    @course = Course.find(params[:course_id])
-    @lessons = @course.lessons
+    course = Course.find(params[:course_id])
+    @lessons = course.lessons
     @lesson = @lessons.find(params[:id])
   end
 end
